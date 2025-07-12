17c2febcda22631ca7c814bf87b80947627c7453
Max <marstonvisuals@gmail.com>
2025-07-12 13:02:07 +0100

    feat: add general format

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -4,18 +4,17 @@ return {
     lazy = false,
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "mason-org/mason-lspconfig.nvim",
-    laz=false,
-    config = function ()
+    laz = false,
+    config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "gopls" }
+        ensure_installed = { "lua_ls", "gopls", "ts_ls" },
       })
-    end
+    end,
   },
-
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -24,19 +23,18 @@ return {
       -- Set up the Lua language server
       lspconfig.lua_ls.setup({})
       lspconfig.gopls.setup({})
+      lspconfig.ts_ls.setup({})
 
       -- Keymaps
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 
       vim.diagnostic.config({
         virtual_text = {
-          enabled = true
-
-        }
+          enabled = true,
+        },
       })
-    end
-  }
+    end,
+  },
 }
-
--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,15 +1,14 @@
-
-return{
+return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", 
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  lazy = false,    
-  config = function ()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-    vim.keymap.set('n', 'C-N>', ':Neotree close',{})
-  end
+  lazy = false,
+  config = function()
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+    vim.keymap.set("n", "C-N>", ":Neotree close", {})
+  end,
 }
